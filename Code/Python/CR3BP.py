eb4abb91dc30c23fdf45898fc9420594f6845ba0
larindin <84553123+larindin@users.noreply.github.com>
2024-01-23 09:21:52 -0500

    Editing Jacobian files

--- Code/Python/CR3BP.py
+++ Code/Python/CR3BP.py
@@ -26,26 +26,27 @@ def CR3BP_DEs(x, y, z, vx, vy, vz, mu):
 
 def CR3BP_jacobian(x, y, z, vx, vy, vz, mu):
 
-    jacobian = np.zeros((6, 6))
-
     # Intermediate values
     d = np.array([x+mu, y, z])
     r = np.array([x+mu-1, y, z])
     dmag = np.linalg.norm(d)
     rmag = np.linalg.norm(r)
 
+    dxdx = 1 - (1 - mu)/(dmag**3) - mu/(rmag**3) + 3*(1 - mu)*(x + mu)**2/(dmag**5) + 3*mu*(x - 1 + mu)**2/(rmag**5)
+    dxdy = 3*(1 - mu)*(x + mu)*y/(dmag**5) + 3*mu*(x - 1 + mu)*y/(rmag**5)
+    dxdz = 3*(1 - mu)*(x + mu)*z/(dmag**5) + 3*mu*(x - 1 + mu)*z/(rmag**5)
+    dydx = dxdy
+    dydy = 1 - (1 - mu)/(dmag**3) - mu/(rmag**3) + 3*(1 - mu)*y**2/(dmag**5) + 3*mu*y**2/(rmag**5)
+    dydz = 3*(1 - mu)*y*z/(dmag**5) + 3*mu*y*z/(rmag**5)
+    dzdx = dxdz
+    dzdy = dydz
+    dzdz = 1 - (1 - mu)/(dmag**3) - mu/(rmag**3) + 3*(1 - mu)*z**2/(dmag**5) + 3*mu*z**2/(rmag**5)
 
-    jacobian[0] = np.array([0, 0, 0, 1, 0, 0])
-    jacobian[1] = np.array([0, 0, 0, 0, 1, 0])
-    jacobian[2] = np.array([0, 0, 0, 0, 0, 1])
-    jacobian[3] = np.array([1 - (1 - mu)/dmag**3 + 3*(1 - mu)*(x + mu)**2/dmag**5 - mu/rmag**3 + 3*mu*(x - 1 + mu)**2/rmag**5,
-                            3*(1 - mu)*(x + mu)*y/dmag**5 + 3*mu*(x - 1 + mu)*y/rmag**5,
-                            3*(1 - mu)*(x + mu)*z/dmag**5 + 3*mu*(x - 1 + mu)*z/rmag**5, 0, 2, 0])
-    jacobian[4] = np.array([3*(1 - mu)*(x + mu)*y/dmag**5 + 3*mu*(x - 1 + mu)*y/rmag**5,
-                            1 - (1 - mu)/dmag**3 - mu/rmag**3 + 3*(1 - mu)*y**2/dmag**5 + 3*mu*y**2/rmag**5,
-                            3*(1 - mu)*y*z/dmag**5 + 3*mu*y*z/rmag**5, -2, 0, 0])
-    jacobian[5] = np.array([3*(1 - mu)*(x + mu)*z/dmag**5 + 3*mu*(x - 1 + mu)*z/rmag**5,
-                            3*(1 - mu)*y*z/dmag**5 + 3*mu*y*z/rmag**5,
-                            1 - (1 - mu)/dmag**3 - mu/rmag**3 + 3*(1 - mu)*z**2/dmag**5 + 3*mu*z**2/rmag**5, 0, 0, 0])
+    jacobian = np.array([[0, 0, 0, 1, 0, 0],
+                         [0, 0, 0, 0, 1, 0],
+                         [0, 0, 0, 0, 0, 1],
+                         [dxdx, dxdy, dxdz, 0, 2, 0],
+                         [dydx, dydy, dydz, -2, 0, 0],
+                         [dzdx, dzdy, dzdz, 0, 0, 0]])
     
     return jacobian
--- Code/Python/testing.py
+++ Code/Python/testing.py
@@ -18,60 +18,73 @@ def ode_eq(t, X, mu):
 def ode_eq_STM(t, X, mu):
 
     x, y, z, vx, vy, vz = X[:6]
-    STM = np.reshape(X[6:], (6, 6))
+    STM = X[6:].reshape((6, 6))
 
     state_derivative = CR3BP_DEs(x, y, z, vx, vy, vz, mu)
     jacobian = CR3BP_jacobian(x, y, z, vx, vy, vz, mu)
-    ddtSTM = jacobian @ STM
+    ddtSTM = np.matmul(jacobian, STM)
 
     ddtX = np.concatenate((state_derivative, ddtSTM.flatten()))
 
     return ddtX
 
-if __name__ == "__main__":
 
-    mu = 1.215059e-2
+mu = 1.215059e-2
 
-    simulation_options = {"atol":1e-12, "rtol":1e-12, "args":(mu,)}
+simulation_options = {"atol":1e-12, "rtol":1e-12, "args":(mu,)}
 
-    if 0==0:
+if 0==0:
 
-        residual = np.ones(3)
-        # while np.linalg.norm(residual) >= 1e-12:
+    residual = np.ones(3)
+    # while np.linalg.norm(residual) >= 1e-12:
 
-        #     inputs = guess2integration(guess)
-        #     propagation = scipy.integrate.solve_ivp(**inputs, args=(mu,))
-        #     residual = constraint_function(propagation.y)
+    #     inputs = guess2integration(guess)
+    #     propagation = scipy.integrate.solve_ivp(**inputs, args=(mu,))
+    #     residual = constraint_function(propagation.y)
 
-        #     new_guess = guess - np.linalg.lstsq(constraint_jacobian(guess), residual)
-        #     guess = new_guess
+    #     new_guess = guess - np.linalg.lstsq(constraint_jacobian(guess), residual)
+    #     guess = new_guess
+
+
+initial_state = np.array([0.7783690577, 0, 0, 0, 0.5556648548, 0])
+initial_STM = np.eye(6)
+ICs = np.concatenate((initial_state, initial_STM.flatten()))
+tspan = np.array([0, 3.7214359005])
+
+results = scipy.integrate.solve_ivp(ode_eq_STM, tspan, ICs, **simulation_options)
+
+time = results.t
+output = results.y
+
+final_STM = np.reshape(output[6:, -1], (6, 6))
+
+print(final_STM)
+print(final_STM @ initial_state)
+
+num_points = len(time)
+STM_results = np.zeros((6, num_points))
+for time_index in np.arange(num_points):
+    STM = np.reshape(output[6:, time_index], (6,6))
+    STM_results[:, time_index] = STM @ initial_state
+
+ax = plt.figure().add_subplot(projection="3d")
+ax.plot(STM_results[0, :100], STM_results[1, :100], STM_results[2, :100])
+ax.plot(output[0, :100], output[1, :100], output[2, :100])
+
+plt.show()
     
-    integration_times = np.array([0.2230147974, 3.7214359005, 6.0500729893])
-    initial_xs = np.array([0.9624690577, 0.7783690577, 0.4625690577])
-    initial_vys = np.array([0.7184165432, 0.5556648548, 1.3253326531])
 
-    results = []
+quit()
 
-    for run_index in (0, 1, 2):
+ax = plt.figure().add_subplot()
+ax.set_aspect("equal")
+ax.set_xlabel("X [dimensionless]")
+ax.set_ylabel("Y [dimensionless]")
+ax.set_title("CR3BP Orbits in XY Plane")
+ax.grid(True)
+for run_index in (0, 1, 2):
+    ax.plot(results[run_index].y[0], results[run_index].y[1])
+ax.legend(["JC = 3.3949", "JC = 2.9123", "JC = 2.6655"])
+ax.plot(1 - mu, 0, "ko")
 
-        t_final = integration_times[run_index]
-        initial_x = initial_xs[run_index]
-        initial_vy = initial_vys[run_index]
-
-        initial_conditions = np.array([initial_x, 0, 0, 0, initial_vy, 0])
-
-        result = scipy.integrate.solve_ivp(ode_eq, np.array([0, t_final]), initial_conditions, **simulation_options)
-        results.append(result)
-
-    ax = plt.figure().add_subplot()
-    ax.set_aspect("equal")
-    ax.set_xlabel("X [dimensionless]")
-    ax.set_ylabel("Y [dimensionless]")
-    ax.set_title("CR3BP Orbits in XY Plane")
-    ax.grid(True)
-    for run_index in (0, 1, 2):
-        ax.plot(results[run_index].y[0], results[run_index].y[1])
-    ax.legend(["JC = 3.3949", "JC = 2.9123", "JC = 2.6655"])
-    ax.plot(1 - mu, 0, "ko")
-
-    plt.show()
+plt.show()
