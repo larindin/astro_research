Fixed Jacobians

Code/Python/CR3BP_pontryagin.py
@@ -1,7 +1,7 @@
 
 
 import numpy as np
-import scipy.integrate
+import scipy
 from CR3BP import *
 
 
@@ -35,7 +35,8 @@ def minimum_fuel_ODE(t, X, mu, umax, rho):
     B = np.vstack((np.zeros((3, 3)), np.eye(3)))
 
     p = -B.T @ costate
-    control = umax/2 * (1 + np.tanh((np.linalg.norm(p) - 1)/rho)) * p/np.linalg.norm(p)
+    G = umax/2 * (1 + np.tanh((np.linalg.norm(p) - 1)/rho))
+    control = G * p/np.linalg.norm(p)
 
     ddt_state_kepler = CR3BP_DEs(t, state, mu)
     ddt_state = ddt_state_kepler + B @ control
@@ -140,8 +141,11 @@ def minimum_fuel_jacobian(state, costate, mu, umax, rho):
     dmag = np.linalg.norm(d)
     rmag = np.linalg.norm(r)
     lnorm = np.linalg.norm(np.array([l4, l5, l6]))
-
     B = np.vstack((np.zeros((3, 3)), np.eye(3)))
+    p = -B.T @ costate
+    S = np.linalg.norm(p) - 1
+    G = umax/2 * (1 + np.tanh(S/rho))
+    my_invcosh = lambda x: 1/np.cosh(x) if abs(x) < 700 else 0
 
     dxdx = 1 - (1 - mu)/(dmag**3) - mu/(rmag**3) + 3*(1 - mu)*(x + mu)**2/(dmag**5) + 3*mu*(x - 1 + mu)**2/(rmag**5)
     dxdy = 3*(1 - mu)*(x + mu)*y/(dmag**5) + 3*mu*(x - 1 + mu)*y/(rmag**5)
@@ -172,20 +176,10 @@ def minimum_fuel_jacobian(state, costate, mu, umax, rho):
     dzdzdy = dydzdz
     dzdzdz = 3*(1 - mu)*z/dmag**5 + 3*mu*z/rmag**5 + 6*(1 - mu)*z/dmag**5 - 15*(1 - mu)*z**3/dmag**7 + 6*mu*z/rmag**5 - 15*mu*z**3/rmag**7
 
+    dGdl4 = umax/rho/2 * my_invcosh(S/rho)**2 * l4/lnorm
+    dGdl5 = umax/rho/2 * my_invcosh(S/rho)**2 * l5/lnorm
+    dGdl6 = umax/rho/2 * my_invcosh(S/rho)**2 * l6/lnorm
 
-    dXdX = np.array([[0, 0, 0, 1, 0, 0],
-                    [0, 0, 0, 0, 1, 0],
-                    [0, 0, 0, 0, 0, 1],
-                    [dxdx, dxdy, dxdz, 0, 2, 0],
-                    [dydx, dydy, dydz, -2, 0, 0],
-                    [dzdx, dzdy, dzdz, 0, 0, 0]])
-    
-    p = -B.T @ costate
-    S = np.linalg.norm(p) - 1
-    G = umax/2 * (1 + np.tanh(S/rho))
-    dGdl4 = umax/2 * 1/rho/np.cosh(S/rho)**2 * l4/lnorm
-    dGdl5 = umax/2 * 1/rho/np.cosh(S/rho)**2 * l5/lnorm
-    dGdl6 = umax/2 * 1/rho/np.cosh(S/rho)**2 * l6/lnorm
     dxdl4 = (-1/lnorm + l4**2/lnorm**3)*G + (-l4/lnorm)*dGdl4
     dxdl5 = (l4*l5/lnorm**3)*G + (-l4/lnorm)*dGdl5
     dxdl6 = (l4*l6/lnorm**3)*G + (-l4/lnorm)*dGdl6
@@ -195,15 +189,23 @@ def minimum_fuel_jacobian(state, costate, mu, umax, rho):
     dzdl4 = (l6*l4/lnorm**3)*G + (-l6/lnorm)*dGdl4
     dzdl5 = (l6*l5/lnorm**3)*G + (-l6/lnorm)*dGdl5
     dzdl6 = (-1/lnorm + l6**2/lnorm**3)*G + (-l6/lnorm)*dGdl6
+    
+    dXdX = np.array([[0, 0, 0, 1, 0, 0],
+                    [0, 0, 0, 0, 1, 0],
+                    [0, 0, 0, 0, 0, 1],
+                    [dxdx, dxdy, dxdz, 0, 2, 0],
+                    [dydx, dydy, dydz, -2, 0, 0],
+                    [dzdx, dzdy, dzdz, 0, 0, 0]])
+    
     dXdL_small = np.array([[dxdl4, dxdl5, dxdl6],
                            [dydl4, dydl5, dydl6],
                            [dzdl4, dzdl5, dzdl6]])
-    dXdL = np.vstack((np.zeros((3, 6)), np.hstack((np.zeros((3, 3)), dXdL_small))))
+    dXdL = scipy.linalg.block_diag(np.zeros((3, 3)), dXdL_small)
 
     dLdX_small = -np.array([[l4*dxdxdx + l5*dxdydx + l6*dxdzdx, l4*dxdxdy + l5*dxdydy + l6*dxdzdy, l4*dxdxdz + l5*dxdydz + l6*dxdzdz],
                             [l4*dxdydx + l5*dydydx + l6*dydzdx, l4*dxdydy + l5*dydydy + l6*dydzdy, l4*dxdydz + l5*dydydz + l6*dydzdz],
                             [l4*dxdzdx + l5*dydzdx + l6*dzdzdx, l4*dxdzdy + l5*dydzdy + l6*dzdzdy, l4*dxdzdz + l5*dydzdz + l6*dzdzdz]])
-    dLdX = np.vstack((np.hstack((dLdX_small, np.zeros((3, 3)))), np.zeros((3, 6))))
+    dLdX = scipy.linalg.block_diag(dLdX_small, np.zeros((3, 3)))
     
     dLdL = -dXdX.T
 

Code/Python/configuration.py
@@ -4,22 +4,26 @@ from measurement_functions import *
 from CR3BP_pontryagin import *
 
 # Truth parameters
-# initial_truth = np.array([5.700765369968086027e-01, 0, 0, 0, 1.001309137115701908e+00, 0, 1.725410447851861151e-01, 1.995473951467236374e-03, 0, 2.043043419011393320e-03, 7.594073607095458422e-02, 0])
-initial_truth = np.array([1.023860, 0, -0.183349, 0, -0.107237, 0, 1.005271672718543563e+00, -4.867201096742823641e-01, 1.039996619651046172e+00, 6.123775200273015029e-01, 5.309094110569010372e-01, 5.410185901250754714e-01])
-# initial_truth = np.array([0.869093134528914, 0, 0, 0, 0.471129523484998, 0, 2.728393465768669479e+00,-2.213062564132921128e+00, -1.368370228190690652e+00, 9.316407006623674825e-01, -2.255988376284393804e-01, 3.620024666599501173e-01])
-# final_time = 6.185934071545815982
-final_time = 2.199798443376498547
-final_time = 0.1
-# final_time = 2.346224101773999760
+# initial_truth = np.array([5.700765369968086027e-01, 0, 0, 0, 1.001309137115701908e+00, 0, 2.218985298179385168e+00, 5.173659581424284309e-03, 0, 1.045748103035356626e-02, 1.002278884441901052e+00, 0])
+initial_truth = np.array([1.023860, 0, -0.183349, 0, -0.107237, 0, 1.886804244746727921e+00, -5.756815826039763939e-01, 4.471842695737356377e-01, 8.068657163041901281e-01, 7.386444568749661599e-01, 5.395507048947811857e-01])
+# initial_truth = np.array([0.869093134528914, 0, 0, 0, 0.471129523484998, 0, 1.886994686356553652e+00, -1.834644038828200818e+00, -1.353616598123221770e+00, 6.170104458498792965e-01, 1.853008581517654740e-01, 7.634714087832630280e-01])
+# final_time = 6.235577707735169284
+final_time = 2.209568031669125077
+# final_time = 1.992457188219079134
+# final_time = 1
+# dt = 30*60/3.751903e5
 dt = 0.01
-dynamics_equation = minimum_energy_ODE
+dynamics_equation = minimum_fuel_ODE
+# initial_truth[6:12] = np.array([0, 0, 0, 1e-12, 1e-12, 1e-12])
+truth_rho = 1e-4
 umax = 1
 mu = 1.215059e-2
+truth_dynamics_args = (mu, umax, truth_rho)
 
 # Measurement parameters
 measurement_equation = az_el_sensor
 measurement_jacobian = az_el_sensor_jacobian
-measurement_noise_covariance = np.eye(2)*np.deg2rad(18/60/60)**2
+measurement_noise_covariance = np.eye(2)*np.deg2rad(1e-3)**2
 measurement_dimension = 2
 individual_measurement_size = 2
 sensor_initial_conditions = np.array([[9.251388163276373922e-01,0,2.188093146262887201e-01,0,1.215781574069972060e-01,0],
@@ -31,7 +35,7 @@ sun_exclusion_angle = np.deg2rad(20)
 seed = 0
 
 # Filter parameters
-initial_covariance = np.eye(6)*0.3**2
+initial_covariance = scipy.linalg.block_diag(np.eye(3)*1.30072841e-4**2, np.eye(3)*9.76041363e-4**2)
 initial_estimate = np.random.default_rng(seed).multivariate_normal(initial_truth[0:6], initial_covariance)
-process_noise_covariance = np.eye(6)*0.15**2
+process_noise_covariance = scipy.linalg.block_diag(np.eye(3)*1e-6**2, np.eye(3)*0.01**2)
 filter_measurement_covariance = measurement_noise_covariance * (1.5)**2

Code/Python/configuration_dual_GMIMM.py
@@ -41,7 +41,7 @@ generator = np.random.default_rng(seed)
 initial_estimate = np.concatenate((generator.multivariate_normal(initial_truth[0:6], initial_state_covariance), np.zeros(3), np.ones(3)*1e-9))
 # initial_estimate = initial_truth
 filter_measurement_covariance = measurement_noise_covariance * (1.1)**2
-filter_rho = 1e-2
+filter_rho = 1e-4
 switching_cutoff = 5
 
 # IMM parameters
@@ -52,10 +52,12 @@ initial_mode_probabilities = np.array([1, 0])
 mode_transition_matrix = np.array([[0.99, 0.01], [0.2, 0.8]])
 
 # GMM parameters
+initial_kernel_state_covariance = scipy.linalg.block_diag(np.eye(3)*1e-6**2, np.eye(3)*1e-3**2)
 initial_kernel_costate_covariance = np.eye(6)*0.01**2
-initial_kernel_covariance = scipy.linalg.block_diag(initial_state_covariance, initial_kernel_costate_covariance)
-kernel_process_noise = scipy.linalg.block_diag(np.eye(3)*(1e-6)**2, np.eye(3)*(1e-3)**2, np.eye(6)*(0.01)**2)
-magnitudes = np.linspace(1.0, 1.20, 21)
+initial_kernel_covariance = scipy.linalg.block_diag(initial_kernel_state_covariance, initial_kernel_costate_covariance)
+kernel_process_noise = scipy.linalg.block_diag(np.eye(3)*(1e-6)**2, np.eye(3)*(1e-3)**2, np.eye(6)*(0.02)**2)
+# magnitudes = np.linspace(1.1, 1.3, 21)
+magnitudes = np.linspace(1.0, 1.2, 21)
 # magnitudes = [1.05]
 num_kernels = len(magnitudes)
 initial_weights = np.ones(num_kernels)/num_kernels

Code/Python/dual_filter.py
@@ -23,11 +23,13 @@ def run_dual_filter(initial_estimate, initial_covariance,
     num_measurements = np.size(measurement_vals, 1)
 
     anterior_estimate_vals = np.empty((state_size, num_measurements))
-    posterior_estimate_vals = np.empty((state_size, num_measurements+1))
+    posterior_estimate_vals = np.empty((state_size, num_measurements))
     anterior_covariance_vals = np.empty((state_size, state_size, num_measurements))
-    posterior_covariance_vals = np.empty((state_size, state_size, num_measurements+1))
+    posterior_covariance_vals = np.empty((state_size, state_size, num_measurements))
     innovations_vals = np.empty((measurement_size, num_measurements))
 
+    anterior_estimate_vals[:, 0] = initial_estimate
+    anterior_covariance_vals[:, :, 0] = initial_covariance
     posterior_estimate_vals[:, 0] = initial_estimate
     posterior_covariance_vals[:, :, 0] = initial_covariance
 
@@ -40,10 +42,10 @@ def run_dual_filter(initial_estimate, initial_covariance,
 
     active_filter_index = filter_index
 
-    for time_index in np.arange(1, num_measurements + 1):
+    for time_index in np.arange(1, num_measurements):
 
-        measurement = measurement_vals[:, time_index-1]
-        current_time = time_vals[time_index-1]
+        measurement = measurement_vals[:, time_index]
+        current_time = time_vals[time_index]
         timespan = current_time - previous_time
 
         if np.array_equal(measurement, np.empty(measurement_size)*np.nan, equal_nan=True):
@@ -97,9 +99,9 @@ def run_dual_filter(initial_estimate, initial_covariance,
 
             
         
-        anterior_estimate_vals[:, time_index-1] = anterior_estimate
+        anterior_estimate_vals[:, time_index] = anterior_estimate
         posterior_estimate_vals[:, time_index] = posterior_estimate
-        anterior_covariance_vals[:, :, time_index-1] = anterior_covariance
+        anterior_covariance_vals[:, :, time_index] = anterior_covariance
         posterior_covariance_vals[:, :, time_index] = posterior_covariance
         # innovations_vals[:, time_index] = innovations
 

Code/Python/test_EKF.py
@@ -6,36 +6,80 @@ from configuration import *
 from CR3BP import *
 from CR3BP_pontryagin import *
 from EKF import *
-from dynamics_functions import *
 from measurement_functions import *
 from plotting import *
 
-time_vals = np.arange(0, final_time, 0.001)
+time_vals = np.arange(0, final_time, dt)
 tspan = np.array([time_vals[0], time_vals[-1]])
-truth_propagation = scipy.integrate.solve_ivp(dynamics_equation, tspan, initial_truth, args=(mu, umax), t_eval=time_vals, atol=1e-12, rtol=1e-12)
+truth_propagation = scipy.integrate.solve_ivp(dynamics_equation, tspan, initial_truth, args=truth_dynamics_args, t_eval=time_vals, atol=1e-12, rtol=1e-12)
 truth_vals = truth_propagation.y
 
-measurements = generate_measurements(time_vals, truth_vals, measurement_equation, measurement_dimension, measurement_noise_covariance, (mu,), seed)
+sensor_position_vals = generate_sensor_positions(sensor_dynamics_equation, sensor_initial_conditions, (mu,), time_vals)
+
+num_sensors = int(np.size(sensor_position_vals, 0)/3)
+earth_vectors = np.empty((3*num_sensors, len(time_vals)))
+moon_vectors = np.empty((3*num_sensors, len(time_vals)))
+sun_vectors = np.empty((3*num_sensors, len(time_vals)))
+for sensor_index in np.arange(num_sensors):
+    sensor_positions = sensor_position_vals[sensor_index*3:(sensor_index + 1)*3, :]
+    earth_vectors[sensor_index*3:(sensor_index + 1)*3, :] = generate_earth_vectors(time_vals, sensor_positions)
+    moon_vectors[sensor_index*3:(sensor_index + 1)*3, :] = generate_moon_vectors(time_vals, sensor_positions)
+    sun_vectors[sensor_index*3:(sensor_index + 1)*3, :] = generate_sun_vectors(time_vals, 0)
+
+earth_results = np.empty((num_sensors, len(time_vals)))
+moon_results = np.empty((num_sensors, len(time_vals)))
+sun_results = np.empty((num_sensors, len(time_vals)))
+check_results = np.empty((num_sensors, len(time_vals)))
+for sensor_index in np.arange(num_sensors):
+    sensor_positions = sensor_position_vals[sensor_index*3:(sensor_index + 1)*3, :]
+    earth_results[sensor_index, :] = check_validity(time_vals, truth_vals[0:3, :], sensor_positions, earth_vectors[sensor_index*3:(sensor_index+1)*3, :], check_exclusion, (earth_exclusion_angle,))
+    moon_results[sensor_index, :] = check_validity(time_vals, truth_vals[0:3, :], sensor_positions, moon_vectors[sensor_index*3:(sensor_index+1)*3, :], check_exclusion_dynamic, (9.0400624349e-3, moon_additional_angle))
+    sun_results[sensor_index, :] = check_validity(time_vals, truth_vals[0:3, :], sensor_positions, sun_vectors[sensor_index*3:(sensor_index+1)*3, :], check_exclusion, (sun_exclusion_angle,))
+    check_results[sensor_index, :] = earth_results[sensor_index, :] * moon_results[sensor_index, :] * sun_results[sensor_index, :]
+
+# check_results[0, -100:] = 0
+# check_results[1, -100:] = 0
+
+# check_results[0, :25] = 0
+# check_results[1, :25] = 1
+
+# check_results[:, 0:100] = 1
+# check_results[:, 100:] = 0
+
+check_results[:, :] = 1
+
+# check_results[:, 50:] = 0
+# check_results[:, 100:] = 1
+# check_results[:, 150:] = 0
+
+measurements = generate_sensor_measurements(time_vals, truth_vals, measurement_equation, individual_measurement_size, measurement_noise_covariance, sensor_position_vals, check_results, seed)
 
 dynamics_args = (mu, )
-measurement_args = (mu, )
+measurement_args = (mu, sensor_position_vals, individual_measurement_size)
 
-def EKF_dynamics_equation(t, X, mu, process_noise_covariance):
+def EKF_dynamics_equation(t, X, mu):
 
     state = X[0:6]
-    covariance = X[6:42].reshape((6, 6))
+    STM = X[6:42].reshape((6, 6))
 
     jacobian = CR3BP_jacobian(state, mu)
 
-    ddt_state = CR3BP_DEs(state, mu)
-    ddt_covariance = jacobian @ covariance + covariance @ jacobian.T + process_noise_covariance
+    ddt_state = CR3BP_DEs(t, state, mu)
+    ddt_covariance = jacobian @ STM
 
     return np.concatenate((ddt_state, ddt_covariance.flatten()))
     
-def EKF_measurement_equation(X, mu):
+def EKF_measurement_equation(time_index, X, mu, sensor_position_vals, individual_measurement_size):
 
-    measurement = az_el_moon(X, mu)
-    measurement_jacobian = az_el_moon_jacobian(X, mu)
+    num_sensors = int(np.size(sensor_position_vals, 0)/3)
+    measurement = np.empty(num_sensors*individual_measurement_size)
+    measurement_jacobian = np.empty((num_sensors*individual_measurement_size, 6))
+
+    for sensor_index in np.arange(num_sensors):
+        sensor_position = sensor_position_vals[sensor_index*3:(sensor_index+1)*3, time_index]
+        
+        measurement[sensor_index*individual_measurement_size:(sensor_index+1)*individual_measurement_size] = az_el_sensor(X, sensor_position)
+        measurement_jacobian[sensor_index*individual_measurement_size:(sensor_index+1)*individual_measurement_size] = az_el_sensor_jacobian(X, sensor_position)
 
     return measurement, measurement_jacobian
 
@@ -71,6 +115,6 @@ posterior_covariances = [posterior_covariance_vals]
 
 estimation_errors = compute_estimation_errors(truth_vals, posterior_estimates, 6)
 three_sigmas = compute_3sigmas(posterior_covariances, 6)
-plot_3sigma(time_vals, estimation_errors, three_sigmas, 1, 1, 6)
+plot_3sigma(time_vals, estimation_errors, three_sigmas, 6, [-0.5, 0.5], 0.25)
 
 plt.show()

Code/Python/test_dual_GMIMM.py
@@ -50,8 +50,9 @@ for sensor_index in np.arange(num_sensors):
 
 check_results[:, :] = 1
 
-# check_results[:, 200:] = 0
-# check_results[:, 300:] = 1
+check_results[:, 50:] = 0
+check_results[:, 100:] = 1
+check_results[:, 150:] = 0
 
 measurements = generate_sensor_measurements(time_vals, truth_vals, measurement_equation, individual_measurement_size, measurement_noise_covariance, sensor_position_vals, check_results, seed)
 
@@ -68,6 +69,19 @@ def coasting_dynamics_equation(t, X, mu, umax, rho):
     ddt_STM = jacobian @ STM
 
     return np.concatenate((ddt_state, ddt_costate, ddt_STM.flatten()))
+
+def min_energy_dynamics_equation(t, X, mu, umax, rho):
+    
+    state = X[0:6]
+    costate = X[6:12]
+    STM = X[12:156].reshape((12, 12))
+
+    jacobian = minimum_energy_jacobian(state, costate, mu, umax)
+
+    ddt_state = minimum_fuel_ODE(0, X[0:12], mu, umax, rho)
+    ddt_STM = jacobian @ STM
+
+    return np.concatenate((ddt_state, ddt_STM.flatten()))
 
 def thrusting_dynamics_equation(t, X, mu, umax, rho):
 
@@ -99,7 +113,7 @@ def filter_measurement_equation(time_index, X, mu, sensor_position_vals, individ
 
 dynamics_args = (mu, umax, filter_rho)
 measurement_args = (mu, sensor_position_vals, individual_measurement_size)
-dynamics_equations = [coasting_dynamics_equation, thrusting_dynamics_equation]
+dynamics_equations = [coasting_dynamics_equation, min_energy_dynamics_equation]
 num_modes = len(dynamics_equations)
 
 
@@ -112,14 +126,6 @@ IMM_t = IMM_output.t
 IMM_posterior_estimate_vals = IMM_output.posterior_estimate_vals
 IMM_posterior_covariance_vals = IMM_output.posterior_covariance_vals
 IMM_weights = IMM_output.weight_vals
-
-
-ax = plt.figure().add_subplot()
-ax.step(IMM_t, IMM_weights[0], alpha=0.5)
-ax.step(IMM_t, IMM_weights[1], alpha=0.5)
-
-plt.show()
-quit()
 
 thrusting_indices = get_thrusting_indices(IMM_output, switching_cutoff)
 start_index = thrusting_indices[0]
@@ -220,6 +226,7 @@ for kernel_index in np.arange(num_kernels):
     initial_estimates[:, kernel_index] = np.concatenate((initial_state, initial_costate_estimates[:, kernel_index]))
     initial_covariances[0:6, 0:6, kernel_index] = initial_covariance
     initial_covariances[6:12, 6:12, kernel_index] = initial_kernel_costate_covariance
+    initial_covariances[:, :, kernel_index] = initial_kernel_covariance
 
 # initial_estimates[:, 0] = truth_vals[:, end_index]
 # initial_covariances[:, :, 0] = np.eye(12)*0.001**2
@@ -256,6 +263,32 @@ ax = plt.figure().add_subplot()
 ax.plot([0, final_time], [0.5, 0.5], alpha=0.5)
 for mode_index in np.arange(num_kernels):
     ax.step(GM_time, weight_vals[mode_index, :], alpha=0.25)
+
+fig = plt.figure()
+ax = fig.add_subplot(231)
+ax.plot(time_vals, truth_vals[0])
+for index in np.arange(num_kernels):
+    ax.step(GM_time, posterior_estimate_vals[0, :, index], alpha=0.25)
+ax = fig.add_subplot(232)
+ax.plot(time_vals, truth_vals[1])
+for index in np.arange(num_kernels):
+    ax.step(GM_time, posterior_estimate_vals[1, :, index], alpha=0.25)
+ax = fig.add_subplot(233)
+ax.plot(time_vals, truth_vals[2])
+for index in np.arange(num_kernels):
+    ax.step(GM_time, posterior_estimate_vals[2, :, index], alpha=0.25)
+ax = fig.add_subplot(234)
+ax.plot(time_vals, truth_vals[3])
+for index in np.arange(num_kernels):
+    ax.step(GM_time, posterior_estimate_vals[3, :, index], alpha=0.25)
+ax = fig.add_subplot(235)
+ax.plot(time_vals, truth_vals[4])
+for index in np.arange(num_kernels):
+    ax.step(GM_time, posterior_estimate_vals[4, :, index], alpha=0.25)
+ax = fig.add_subplot(236)
+ax.plot(time_vals, truth_vals[5])
+for index in np.arange(num_kernels):
+    ax.step(GM_time, posterior_estimate_vals[5, :, index], alpha=0.25)
 
 fig = plt.figure()
 ax = fig.add_subplot(231)
@@ -301,5 +334,14 @@ ax = fig.add_subplot(313)
 ax.step(time_vals, truth_control[2])
 for index in np.arange(num_kernels):
     ax.step(GM_time, estimated_controls[index][2], alpha=0.25)
+
+truth_norm_vals = np.linalg.norm(truth_vals[9:12], axis=0)
+norm_vals = np.empty((num_kernels, len(GM_time)))
+for index in np.arange(num_kernels):
+    norm_vals[index, :] = np.linalg.norm(posterior_estimate_vals[9:12, :, index], axis=0)
+ax = plt.figure().add_subplot()
+ax.plot(time_vals, truth_norm_vals)
+for index in np.arange(num_kernels):
+    ax.plot(GM_time, norm_vals[index], alpha=0.25)
 
 plt.show()

Code/Python/test_s2s_fuel.py
@@ -60,13 +60,12 @@ measurements = generate_sensor_measurements(time_vals, truth_vals, measurement_e
 def EKF_dynamics_equation(t, X, mu, process_noise_covariance):
 
     state = X[0:6]
-    covariance = X[6:42].reshape((6, 6))
+    STM = X[6:42].reshape((6, 6))
 
     jacobian = CR3BP_jacobian(state, mu)
 
     ddt_state = CR3BP_DEs(t, state, mu)
-    ddt_covariance = jacobian @ covariance + covariance @ jacobian.T + process_noise_covariance
-
+    ddt_covariance = jacobian @ STM
     return np.concatenate((ddt_state, ddt_covariance.flatten()))
     
 def EKF_measurement_equation(time_index, X, mu, sensor_position_vals, individual_measurement_size):
@@ -83,16 +82,16 @@ def EKF_measurement_equation(time_index, X, mu, sensor_position_vals, individual
 
     return measurement, measurement_jacobian
 
-def costate_dynamics_equation(t, X, mu, umax, rho, process_noise_covariance):
+def costate_dynamics_equation(t, X, mu, umax, rho):
 
     state = X[0:6]
     costate = X[6:12]
-    covariance = X[12:156].reshape((12, 12))
+    STM = X[12:156].reshape((12, 12))
 
-    jacobian = CR3BP_costate_jacobian(state, costate, mu, umax)
+    jacobian = minimum_fuel_jacobian(state, costate, mu, umax, rho)
 
     ddt_state = minimum_fuel_ODE(0, X[0:12], mu, umax, rho)
-    ddt_covariance = jacobian @ covariance + covariance @ jacobian.T + process_noise_covariance
+    ddt_covariance = jacobian @ STM
 
     return np.concatenate((ddt_state, ddt_covariance.flatten()))
     