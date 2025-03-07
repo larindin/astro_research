Fixed backprop bug and fixed VSDF

Code/Python/VSDF.py
@@ -136,6 +136,7 @@ class VSD_filter():
             size = self.maneuvering_size
 
         innovations_covariance = measurement_jacobian @ anterior_covariance @ measurement_jacobian.T + measurement_noise_covariance
+        innovations_covariance = enforce_symmetry(innovations_covariance)
         cross_covariance = anterior_covariance @ measurement_jacobian.T
         gain_matrix = cross_covariance @ np.linalg.inv(innovations_covariance)
 
@@ -143,7 +144,7 @@ class VSD_filter():
         # for sensor_index, r in enumerate(rs):
         #     innovations[sensor_index*3:(sensor_index+1)*3]*= r
         posterior_estimate[0:size] = anterior_estimate + gain_matrix @ innovations
-        posterior_covariance [0:size, 0:size]= anterior_covariance - cross_covariance @ gain_matrix.T - gain_matrix @ cross_covariance.T + gain_matrix @ innovations_covariance @ gain_matrix.T
+        posterior_covariance [0:size, 0:size]= enforce_symmetry(anterior_covariance - cross_covariance @ gain_matrix.T - gain_matrix @ cross_covariance.T + gain_matrix @ innovations_covariance @ gain_matrix.T)
 
         innovations_distance = innovations.T @ np.linalg.inv(innovations_covariance) @ innovations
 
@@ -170,7 +171,7 @@ class VSD_filter():
 
         STM = propagation[size:size**2 + size].reshape((size, size))
         anterior_estimate[0:size] = propagation[0:size]
-        anterior_covariance[0:size, 0:size] = STM @ posterior_covariance @ STM.T + self.process_noise_covariances[active_filter]
+        anterior_covariance[0:size, 0:size] = enforce_symmetry(STM @ posterior_covariance @ STM.T + self.process_noise_covariances[active_filter])
 
         return anterior_estimate, anterior_covariance
 

Code/Python/test_VSDF.py
@@ -17,11 +17,11 @@ forprop_time_vals = np.arange(0, final_time, dt)
 backprop_tspan = np.array([backprop_time_vals[0], backprop_time_vals[-1]])
 forprop_tspan = np.array([forprop_time_vals[0], forprop_time_vals[-1]])
 back_propagation = scipy.integrate.solve_ivp(CR3BP_DEs, backprop_tspan, initial_truth[0:6], args=(mu,), t_eval=backprop_time_vals, atol=1e-12, rtol=1e-12).y
-back_propagation = np.vstack((back_propagation, np.ones(np.shape(back_propagation))*1e-9))
+back_propagation = np.vstack((back_propagation, np.full(np.shape(back_propagation), np.nan)))
 back_propagation = np.flip(back_propagation, axis=1)
 forward_propagation = scipy.integrate.solve_ivp(dynamics_equation, forprop_tspan, initial_truth, args=truth_dynamics_args, t_eval=forprop_time_vals, atol=1e-12, rtol=1e-12).y
-truth_vals = np.concatenate((back_propagation, forward_propagation), axis=1)
-time_vals = np.arange(0, final_time+backprop_time, dt)
+truth_vals = np.concatenate((back_propagation[:, :-1], forward_propagation), axis=1)
+time_vals = np.concatenate((np.flip(backprop_time_vals[1:]), forprop_time_vals)) + abs(backprop_time_vals[-1])
 
 initial_estimate = generator.multivariate_normal(truth_vals[0:6, 0], initial_state_covariance)
 
@@ -150,7 +150,7 @@ def PV_measurement_equation(time_index, X, measurement_variances, sensor_positio
 
 def q2m_initialization(posterior_estimate, posterior_covariance):
     posterior_estimate[6:12] = 0
-    posterior_covariance = scipy.linalg.block_diag(posterior_covariance[0:6, 0:6], np.eye(6)*1**2)
+    posterior_covariance = scipy.linalg.block_diag(posterior_covariance[0:6, 0:6], np.eye(6)*1e1**2)
     return posterior_estimate, posterior_covariance
 
 
@@ -159,7 +159,7 @@ filter_measurement_function = angles_measurement_equation
 # measurements = angles2PV(measurements)
 
 quiescent_ODE_args = (mu,)
-maneuvering_ODE_args = (mu, umax)
+maneuvering_ODE_args = (mu, 5)
 measurement_args = (measurement_variances, sensor_position_vals, check_results)
 
 filter = VSD_filter(quiescent_ODE,
@@ -184,7 +184,6 @@ truth_control = get_min_fuel_control(truth_vals[6:12, :], umax, truth_rho)
 estimated_control = get_min_energy_control(posterior_estimate_vals[6:12], umax)
 
 ax = plt.figure().add_subplot(projection="3d")
-
 ax.plot(truth_vals[0], truth_vals[1], truth_vals[2], alpha=0.5)
 ax.plot(posterior_estimate_vals[0], posterior_estimate_vals[1], posterior_estimate_vals[2], alpha=0.5)
 ax.set_aspect("equal")

Code/Python/test_initial_filter_algorithm.py
@@ -20,11 +20,11 @@ forprop_time_vals = np.arange(0, final_time, dt)
 backprop_tspan = np.array([backprop_time_vals[0], backprop_time_vals[-1]])
 forprop_tspan = np.array([forprop_time_vals[0], forprop_time_vals[-1]])
 back_propagation = scipy.integrate.solve_ivp(CR3BP_DEs, backprop_tspan, initial_truth[0:6], args=(mu,), t_eval=backprop_time_vals, atol=1e-12, rtol=1e-12).y
-back_propagation = np.vstack((back_propagation, np.ones(np.shape(back_propagation))*1e-9))
+back_propagation = np.vstack((back_propagation, np.full(np.shape(back_propagation), np.nan)))
 back_propagation = np.flip(back_propagation, axis=1)
 forward_propagation = scipy.integrate.solve_ivp(dynamics_equation, forprop_tspan, initial_truth, args=truth_dynamics_args, t_eval=forprop_time_vals, atol=1e-12, rtol=1e-12).y
-truth_vals = np.concatenate((back_propagation, forward_propagation), axis=1)
-time_vals = np.arange(0, final_time+backprop_time, dt)
+truth_vals = np.concatenate((back_propagation[:, :-1], forward_propagation), axis=1)
+time_vals = np.concatenate((np.flip(backprop_time_vals[1:]), forprop_time_vals)) + abs(backprop_time_vals[-1])
 
 initial_estimate = np.concatenate((generator.multivariate_normal(truth_vals[0:6, 0], initial_state_covariance), np.ones(6)*1e-6))
 